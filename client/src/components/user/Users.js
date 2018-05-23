dfdc1c29c1e892c716850cf4fd48672e35020b3a
mauromero <mromeroval@ucdavis.edu>
2018-05-23 13:25:20 -0700

    Intalled react-table and applied it to Users list

--- client/src/components/user/Users.js
+++ client/src/components/user/Users.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
+
 
 class Users extends Component{
 
@@ -13,32 +16,89 @@ class Users extends Component{
             return i;
         }
         
-        const userList = this.props.state.users.map((user,i)=>{
-            var created_at = new Date(user.created_at);
-            var updated_at = new Date(user.updated_at);
-            var created = addZero(created_at.getMonth()+1) +"/"
-                        +addZero(created_at.getDate())+"/"
-                        +addZero(created_at.getFullYear());
-            var updated = addZero(updated_at.getMonth()+1) +"/"
-                        +addZero(updated_at.getDate())+"/"
-                        +addZero(updated_at.getFullYear());
+        // const userList = this.props.state.users.map((user,i)=>{
+        //     var created_at = new Date(user.created_at);
+        //     var updated_at = new Date(user.updated_at);
+        //     var created = addZero(created_at.getMonth()+1) +"/"
+        //                 +addZero(created_at.getDate())+"/"
+        //                 +addZero(created_at.getFullYear());
+        //     var updated = addZero(updated_at.getMonth()+1) +"/"
+        //                 +addZero(updated_at.getDate())+"/"
+        //                 +addZero(updated_at.getFullYear());
                         
-            return (
-                <tr key={ i } >
-                    <th scope="row"> { i+1 } </th>
-                    <td> <Link to={ `/users/${user.id}`}> { user.user_name } </Link> </td>
-                    <td> { user.gender } </td>
-                    <td> { user.provided_languages.map(language=>{
-                                if(language.ability === 5){
-                                    return  <div key={language.language.id}> {language.language.short_name}</div>
-                                }else return null
-                            })}
-                    </td>
-                    <td> { created } </td>  
-                    <td> { updated } </td>  
-                </tr>    
+        //     return (
+        //         <tr key={ i } >
+        //             <th scope="row"> { i+1 } </th>
+        //             <td> <Link to={ `/users/${user.id}`}> { user.user_name } </Link> </td>
+        //             <td> { user.gender } </td>
+        //             <td> { user.provided_languages.map(language=>{
+        //                         if(language.ability === 5){
+        //                             return  <div key={language.language.id}> {language.language.short_name}</div>
+        //                         }else return null
+        //                     })}
+        //             </td>
+        //             <td> { created } </td>  
+        //             <td> { updated } </td>  
+        //         </tr>    
+        //     )
+        // });
+
+        const userList = this.props.state.users.map((user)=>{return user });
+        // const data = [{
+        //     name: 'Tanner Linsley',
+        //     age: 26,
+        //   },{
+        //     name: 'Mia Wallace',
+        //     age: 25,
+        //   }
+        // ];
+
+        const columns = [{
+            Header: 'Show',
+            accessor: 'id',
+            Cell: row => (<a href={ `/users/${row.value}`} className="btn btn-sm btn-primary"> Show</a>)
+
+          },{
+            Header: 'Username',
+            accessor: 'user_name'
+          }, {
+            Header: 'Gender',
+            accessor: 'gender',
+          }, {
+            Header: 'Known Lang.',
+            accessor: 'provided_languages',
+            Cell:  row => (
+                row.value.map(language=>{ 
+                        return(<div key={language.id}>{language.language.short_name}<br/></div>)
+                })
             )
-        });
+          },{
+            Header: 'Learning Lang.',
+            accessor: 'desired_languages',
+            Cell:  row => (
+                row.value.map(language=>{ 
+                        return(<div key={language.id}>{language.language.short_name}<br/></div>)
+                })
+            )
+          },{
+            Header: 'Registered',
+            accessor: 'created_at',
+            Cell:  row => (
+                addZero(new Date(row.value).getMonth()+1) +"/"
+                        +addZero(new Date(row.value).getDate())+"/"
+                        +addZero(new Date(row.value).getFullYear())
+            )
+          },{
+            Header: 'Last Login',
+            accessor: 'updated_at',
+            Cell:  row => (
+                addZero(new Date(row.value).getMonth()+1) +"/"
+                        +addZero(new Date(row.value).getDate())+"/"
+                        +addZero(new Date(row.value).getFullYear())
+            )
+          }]
+
+        
         
 
         return (
@@ -49,21 +109,18 @@ class Users extends Component{
                     </div>
                     <div className="card-body">
                         <div className="table-responsive">
-                            <table className="table table-sm table-hover">
-                            <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Gender</th>
-                                <th scope="col">Native</th>
-                                <th scope="col">Registered</th>
-                                <th scope="col">Last Login</th>   
-                            </tr>
-                            </thead>
-                            <tbody>
-                            { userList }
-                            </tbody>
-                            </table> 
+
+                        <ReactTable
+                            data={userList}
+                            columns={columns}
+                            className="-striped -highlight"
+                            defaultSorted={[
+                                {
+                                  id: "created_at",
+                                  desc: true
+                                }
+                              ]}
+                        />
                         </div>
                     </div>
                 </div>
