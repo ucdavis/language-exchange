f85c3838ff61d45de6738631de18a0d59e0d97d3
Mauricio Romero <mrgrafica@gmail.com>
2018-06-05 23:18:04 -0700

    Modified home page text in cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -31,26 +31,26 @@ class Home extends Component{
                 <div className="card-group">
 
                     <div className="card bg-light mb-3" styles="max-width: 18rem;">
-                        <div className="card-header text-white bg-dark">Find a partner</div>
+                        <div className="card-header text-white bg-dark">Search</div>
                         <div className="card-body">
-                            <h5 className="card-title">Information</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Find a partner</h5>
+                            <p className="card-text">Use the TLE search tool to find your language exchange partner.</p>
                         </div>
                     </div>
 
                     <div className="card bg-light mb-3" styles="max-width: 18rem;">
-                        <div className="card-header bg-warning text-white">Read your Messages</div>
+                        <div className="card-header bg-warning text-white">Contact</div>
                         <div className="card-body">
-                            <h5 className="card-title">Information</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Send Messages</h5>
+                            <p className="card-text">Contact your partner through the message system to establish a meeting.</p>
                         </div>
                     </div>
 
                     <div className="card bg-light mb-3" styles="max-width: 18rem;">
-                        <div className="card-header bg-primary text-white">Improve your Skills</div>
+                        <div className="card-header bg-primary text-white">Improve</div>
                         <div className="card-body">
-                            <h5 className="card-title">Information</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Enhance your Skills</h5>
+                            <p className="card-text">The clearest path to fluency is finding a language exchange partner.</p>
                         </div>
                     </div>    
 
