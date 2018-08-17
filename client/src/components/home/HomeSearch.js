47f654ba89f299b14e2bb323fa63567449d84373
Mauricio Romero <mrgrafica@gmail.com>
2018-08-17 16:08:54 -0700

    Added link to guide in home

--- client/src/components/home/HomeSearch.js
+++ client/src/components/home/HomeSearch.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchUsers from '../../containers/user/SearchUsers';
 import SearchUsersResults from '../../containers/user/SearchUsersResult';
+import {Link} from 'react-router-dom';
 
 class HomeSearch extends Component{
 
@@ -11,7 +12,8 @@ class HomeSearch extends Component{
                 <div className="card mt-3 border-info">
                     <div className="card-body">
                         <h4 className="mt-0">Welcome to Tandem Language Exchange (TLE)</h4>
-                        <p className="lead">This program helps a native speaker of one language to find a native speaker of another language to help each other in language learning</p>
+                        <p className="lead">This program helps a native speaker of one language to find a native speaker of another language to help each other in language learning.
+                        <Link to={'/users/guide'}>This is how it works.</Link></p>
                     </div>
                 </div>
 
