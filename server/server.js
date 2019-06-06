d27f5a842933bb288c9f8d8b7f502f44a201ffae
Mauricio Romero <mromeroval@ucdavis.edu>
2019-06-06 10:08:10 -0700

    Cleaned unused code and packages before AWS migration

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
-var nodemailer = require('nodemailer');
+
 var errorHandler = require('strong-error-handler');
 const morgan = require('morgan');
 
@@ -68,7 +68,6 @@ app.use(morgan('combined', {
 let publicDir = 'client/app';
 var envVar = process.env.NODE_ENV;
 
-
 app.get('/', function (req, res) {
   if( envVar == 'maintenance'){ 
     res.sendFile(path.resolve(__dirname, 'client/maintenance', 'index.html'));
@@ -76,19 +75,6 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'client/app', 'index.html'));
   }
 });
-
-
-// Check all requests for authentication
-// app.use(function (req, res, next) {
-//   var user_name = null;
-//   if(user_name = req.session[cas.session_name]) {
-//     console.log("\n Authenticated CAS user: ", user_name);
-//     next();
-//   } else {
-//     // No username in session, need to log in
-//     cas.bounce(req, res, next);       
-//   }
-// });
 
 // This route will de-authenticate the client with the Express server and then 
 // redirect the client to the CAS logout page. 
@@ -101,9 +87,7 @@ app.get( '/logout', function(req,res){
     }
     // Redirect the client to the CAS logout.
     res.redirect(cas_logout);
-
   });
-
 });
 
 
@@ -122,64 +106,6 @@ app.get('/users/*', cas.bounce, function (req, res){
 
 app.get('/admin/*', cas.bounce, function(req,res){
       res.sendFile(path.resolve(__dirname, publicDir, 'index.html'));
-});
-
-
-
-// NODEMAILER
-
-app.get('/email',cas.bounce, function(req,res){
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.ucdavis.edu',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    // auth: {
-    //     user: account.user, // generated ethereal user
-    //     pass: account.pass // generated ethereal password
-    // }
-    // Allow unauthorized domains (when running from localhost)
-    // ,
-    // tls:{
-    //   rejectUnauthorized:false
-    // }
-  });
-
-
-  var output = `<b>UCD Tandem Language Exchange</b> <br/>
-                <p>Hello, this email is to inform you that your status at the TLE web application has changed to "Unavailable" due to long inactivity.
-                <br/>Your data will not show up in search results for others to contact you.
-                <br/>You can edit your profile and check the "Available" box to make yourself available again.</p>
-                <p>Please do not reply to this email, if you have any troubles accessing the system contact our support team at https://harcs-it.ucdavis.edu
-                <br/>
-                <p>UCD Tandem Language Exchange
-                <br/>Find your language exchange partner 😀💬
-                <br/><b>tle.ucdavis.edu</b></p>
-                `
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-    from: '"Language Exchange" <tle-no-reply@ucdavis.edu>', // sender address
-    to: 'somebody@ucdavis.edu', // list of receivers
-    subject: 'TLE Language Exchange Notification', // Subject line
-    text: 'Hello from TLE', // plain text body
-    html: output // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  });
-  res.send("Email Sent!")
-
 });
 
 
